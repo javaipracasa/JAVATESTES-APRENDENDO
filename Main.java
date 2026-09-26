1d0c5de3a36895c25ad0c17c683f4f2a366eb733
Javaipacasa <viniciusdeaquinogomez@gmail.com>
2026-09-26 02:01:26 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,10 +3,20 @@ public class Main {
         Pessoa pessoa1 = new Pessoa("Boo", 20);
         Pessoa pessoa2 = new Pessoa("Carlos", 25);
 
-        System.out.println("Nome: " + pessoa1.nome);
-        System.out.println("Idade: " + pessoa1.idade);
+        pessoa1.apresentar();
+        pessoa2.apresentar();
 
-        System.out.println("Nome: " + pessoa2.nome);
-        System.out.println("Idade: " + pessoa2.idade);
+        pessoa1.falar("Boa noite!");
+        pessoa2.falar("Boa noite para voce tambem!");
+
+        pessoa1.falar("Qual a soma de 10 + 5?");
+        pessoa2.falar("O resultado dessa soma é: ");
+        pessoa2.somar(10, 5);
+
+        pessoa1.falar("Muito esperto.. usou gpt?kkkk agora faça 4 * 5 quero ver");
+        pessoa2.falar("jamais kkkkkk o resultado é:");
+        int multiplicar = pessoa1.multiplicar(5, 4);
+        System.out.println(multiplicar);
+
     }
 }
--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,26 @@
 public class Pessoa {
+
     String nome;
     int idade;
 
     Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
+        int totalPessoas = 0;
 
     }
+        void apresentar() {
+            System.out.println("Meu nome é: " + nome);
+            System.out.println("Tenho " + idade + " anos.");
+
+        }
+        void falar(String mensagem){
+            System.out.println(nome + " Disse: " + mensagem);
+        }
+        void somar(int numero1,int numero2) {
+            System.out.println(numero1 + numero2);
+        }
+        int multiplicar(int numero1, int numero2){
+            return numero1 * numero2;
+        }
 }
